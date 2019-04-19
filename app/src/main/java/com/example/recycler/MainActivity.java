ab66495e4e4421ce9eeb66cd590c22bf2c0a5832
Softwarica College Lab <softwarica@softwarica.com>
2019-04-19 07:20:47 +0545

    continuing

---
+++ app/src/main/java/com/example/recycler/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.recycler;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import adapter.ContactsAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.RecyclerView);
+
+                List<Contacts> contactslist = new ArrayList<>();
+        contactslist.add(new Contacts("Saugat Malla","908011122",R.drawable.saugat));
+        contactslist.add(new Contacts("Rajesh Hamal","908-92212",R.drawable.rajesh));
+        contactslist.add(new Contacts("Dahayang Rai","908092110",R.drawable.dahayang));
+        contactslist.add(new Contacts("Dahayang Rai","908092110",R.drawable.dahayang));
+        contactslist.add(new Contacts("Dahayang Rai","908092110",R.drawable.dahayang));
+        contactslist.add(new Contacts("Dahayang Rai","908092110",R.drawable.dahayang));
+        contactslist.add(new Contacts("Dahayang Rai","908092110",R.drawable.dahayang));
+        contactslist.add(new Contacts("Dahayang Rai","908092110",R.drawable.dahayang));
+        contactslist.add(new Contacts("Dahayang Rai","908092110",R.drawable.dahayang));
+        contactslist.add(new Contacts("Dahayang Rai","908092110",R.drawable.dahayang));
+        contactslist.add(new Contacts("Dahayang Rai","908092110",R.drawable.dahayang));
+        contactslist.add(new Contacts("Dahayang Rai","908092110",R.drawable.dahayang));
+        contactslist.add(new Contacts("Dahayang Rai","908092110",R.drawable.dahayang));
+        contactslist.add(new Contacts("Dahayang Rai","908092110",R.drawable.dahayang));
+        contactslist.add(new Contacts("Bhuwan KC","908092211",R.drawable.bhuwan));
+        contactslist.add(new Contacts("Bhuwan KC","908092211",R.drawable.bhuwan));
+        contactslist.add(new Contacts("Bhuwan KC","908092211",R.drawable.bhuwan));
+        contactslist.add(new Contacts("Bhuwan KC","908092211",R.drawable.bhuwan));
+        contactslist.add(new Contacts("Bhuwan KC","908092211",R.drawable.bhuwan));
+        contactslist.add(new Contacts("Bhuwan KC","908092211",R.drawable.bhuwan));
+        contactslist.add(new Contacts("Bhuwan KC","908092211",R.drawable.bhuwan));
+        contactslist.add(new Contacts("Bhuwan KC","908092211",R.drawable.bhuwan));
+        contactslist.add(new Contacts("Bhuwan KC","908092211",R.drawable.bhuwan));
+        contactslist.add(new Contacts("Bhuwan KC","908092211",R.drawable.bhuwan));
+        contactslist.add(new Contacts("Bhuwan KC","908092211",R.drawable.bhuwan));
+
+
+        ContactsAdapter contactsAdapter=new ContactsAdapter(this,contactslist);
+        recyclerView.setAdapter(contactsAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+    }
+}
